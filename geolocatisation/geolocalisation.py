dd986c61dbdfb8f766bbe8a6bf8a64333a2e5e60
slarinier <slarinier@sekoia.fr>
2015-03-05 10:08:37 +0100

    correct bug

---
+++ geolocatisation/geolocalisation.py
@@ -0,0 +1,27 @@
+'''
+Created on Feb 20, 2013
+
+@author: slarinier
+'''
+from pymongo.connection import Connection
+
+class Geolocalisation(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,list_domaine,db_value):
+        '''
+        Constructor
+        '''
+        
+    def geolochoffline(self):
+      
+                
+    def geolocOnline(self):
+        
+
+    
+            
+            
---
+++ scanners/networks.py
@@ -0,0 +1,44 @@
+'''
+Created on 14 mai 2014
+
+@author: slarinier
+'''
+from libnmap.parser import NmapParser
+from libnmap.process import NmapProcess
+
+class Networks(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, targets,options):
+        self.nmap=NmapProcess(targets,options)
+    def run(self):
+        self.nmap.run()
+        
+    def make_report(self):
+        report=NmapParser.parse(self.nmap.stdout)
+        result=[]
+        for host in report.hosts:
+            temp={}
+            print  host.scripts_results
+            temp['ip']=host.ipv4
+            print [(service.state,service.port,service.scripts_results) for service in host.services]
+#             for service in host.services:
+#                 for k in service.scripts_results:
+#                     if k.find('.'):
+#                         v=service.scripts_results[k]
+#                         del service.scripts_resutls[k]
+#                         service.scripts_resutls[k.replace('.','_')]=v
+#                 temp['services']=[(service.state,service.port,service.scripts_results)]
+#             result.append(temp)
+#         return result
+    def record_report(self,records,cache,coll):
+        for r in records:
+            doc=cache[r['ip']]
+            doc['service']=r
+            try:
+                coll.save(doc)
+            except:
+                print doc
